e0a51f1c89de212b1fd56b0791410d47d18d3507
WillCQ97 <willian.cqueiroz@gmail.com>
2022-06-15 23:01:25 -0300

    Adição das informações dos centros e departamentos

--- bd/0-schema.sql
+++ bd/0-schema.sql
@@ -30,3 +30,51 @@ CREATE TABLE public.metas (
 
 ALTER TABLE public.metas ADD CONSTRAINT metas_fk FOREIGN KEY (id_objetivo) REFERENCES public.objetivos(id);
 
+-- public.unidades definition
+
+-- Drop table
+
+-- DROP TABLE public.unidades;
+
+CREATE TABLE public.unidades (
+	id int4 NOT NULL,
+	unidade varchar NOT NULL,
+	CONSTRAINT unidade_pk PRIMARY KEY (id)
+);
+
+-- public.centros definition
+
+-- Drop table
+
+-- DROP TABLE public.centros;
+
+CREATE TABLE public.centros (
+	id int4 NOT NULL,
+	id_unidade int4 NOT NULL,
+	nome varchar NOT NULL,
+	CONSTRAINT centro_pk PRIMARY KEY (id)
+);
+
+
+-- public.centros foreign keys
+
+ALTER TABLE public.centros ADD CONSTRAINT centros_fk FOREIGN KEY (id_unidade) REFERENCES public.unidades(id);
+
+-- public.departamentos definition
+
+-- Drop table
+
+-- DROP TABLE public.departamentos;
+
+CREATE TABLE public.departamentos (
+	id int4 NOT NULL,
+	id_centro int4 NOT NULL,
+	nome varchar NOT NULL,
+	CONSTRAINT departamento_pk PRIMARY KEY (id)
+);
+
+
+-- public.departamentos foreign keys
+
+ALTER TABLE public.departamentos ADD CONSTRAINT departamento_fk FOREIGN KEY (id_centro) REFERENCES public.centros(id);
+
---
+++ bd/3-unidades.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.unidades (id,unidade) VALUES
+	 (1,'Alegre'),
+	 (2,'Goiabeiras'),
+	 (3,'Maruípe'),
+	 (4,'São Mateus');
