1f8be116d0deb4de0b60927f9e7be5448bbc615d
WillCQ97 <willian.cqueiroz@gmail.com>
2022-09-10 18:02:08 -0300

    Commit inicial

---
+++ SistemaCadastroProjetosODS/src/main/java/br/ufes/scpods/SistemaCadastroProjetosODSApplication.java
@@ -0,0 +1,14 @@
+package br.ufes.scpods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCadastroProjetosODSApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( SistemaCadastroProjetosODSApplication.class, args );
+		System.out.println( "Executei aqui" );
+	}
+
+}
---
+++ SistemaCadastroProjetosODS/src/test/java/br/ufes/scpods/SistemaCadastroProjetosODSApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufes.scpods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCadastroProjetosODSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
