e96eb30ca578e879b1ffa92a9ed681a1aa54efe0
WillCQ97 <willian.cqueiroz@gmail.com>
2022-10-17 10:31:25 -0300

    atualização schema metas e objetivos

--- bd/0-schema.sql
+++ bd/0-schema.sql
@@ -1,10 +1,10 @@
--- public.tb_objetivos_ods definition
+-- public.tb_objetivos definition
 
 -- Drop table
 
--- DROP TABLE public.tb_objetivos_ods;
+-- DROP TABLE public.tb_objetivos;
 
-CREATE TABLE public.tb_objetivos_ods (
+CREATE TABLE public.tb_objetivos (
 	id int4 NOT NULL,
 	titulo varchar NOT NULL,
 	descricao varchar NOT NULL,
@@ -13,17 +13,16 @@ CREATE TABLE public.tb_objetivos_ods (
 );
 
 
--- public.tb_metas_ods definition
+-- public.tb_metas definition
 
 -- Drop table
 
--- DROP TABLE public.tb_metas_ods;
+-- DROP TABLE public.tb_metas;
 
-CREATE TABLE public.tb_metas_ods (
+CREATE TABLE public.tb_metas (
 	id varchar NOT NULL,
 	id_objetivo int4 NOT NULL,
 	descricao varchar NOT NULL,
 	CONSTRAINT tb_meta_ods_pk PRIMARY KEY (id),
-	CONSTRAINT tb_meta_ods_fk FOREIGN KEY (id_objetivo) REFERENCES public.tb_objetivos_ods(id)
+	CONSTRAINT tb_meta_ods_fk FOREIGN KEY (id_objetivo) REFERENCES public.tb_objetivos(id)
 );
-
