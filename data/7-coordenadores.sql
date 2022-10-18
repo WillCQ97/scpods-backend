5a7cae6ade4e7c0f68f12fcc8e3f4fce54547272
WillCQ97 <willian.cqueiroz@gmail.com>
2022-10-18 15:59:48 -0300

    update schema: add coordenadores

--- data/7-coordenadores.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO public.coordenadores_acoes
-(id_tipo_vinculo, nome_completo)
-VALUES
-    (1, 'Leandro Pin'),
-    (1, 'Janaina Cecília Oliveira Villanova'),
-    (1, 'Juliana Severi'),
-    (1, 'Dirceu Pratissoli')
-;
--- schema.sql
+++ schema.sql
@@ -25,3 +25,35 @@ CREATE TABLE public.tb_metas (
 	CONSTRAINT tb_metas_pk PRIMARY KEY (id),
 	CONSTRAINT tb_metas_fk FOREIGN KEY (id_objetivo) REFERENCES public.tb_objetivos(id)
 );
+
+-- public.tb_tipo_vinculo definition
+
+-- Drop table
+
+-- DROP TABLE public.tb_tipo_vinculo;
+
+CREATE TABLE public.tb_tipo_vinculo (
+	id serial4 NOT NULL,
+	vinculo varchar NOT NULL,
+	CONSTRAINT tb_tipo_vinculo_pk PRIMARY KEY (id)
+);
+
+-- public.tb_coordenadores definition
+
+-- Drop table
+
+-- DROP TABLE public.tb_coordenadores;
+
+CREATE TABLE public.tb_coordenadores (
+	id serial4 NOT NULL,
+	id_vinculo int4 NOT NULL,
+	ds_vinculo varchar NULL,
+	nome varchar NOT NULL,
+	CONSTRAINT tb_coordenadores_pk PRIMARY KEY (id)
+);
+
+
+-- public.tb_coordenadores foreign keys
+
+ALTER TABLE public.tb_coordenadores ADD CONSTRAINT tb_coordenadores_fk_vinculo FOREIGN KEY (id_vinculo) REFERENCES public.tb_tipo_vinculo(id);
+
