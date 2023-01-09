90a72bfcbd1ed90f914e51548e7679e60ecf7eb4
WillCQ97 <willian.cqueiroz@gmail.com>
2023-01-09 07:56:30 -0300

    Adição validação com spring starter validation

--- src/main/java/br/ufes/willcq/scpods/domain/model/Acao.java
+++ src/main/java/br/ufes/willcq/scpods/domain/model/Acao.java
@@ -4,11 +4,15 @@ import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.locationtech.jts.geom.Point;
 
@@ -26,15 +30,20 @@ import lombok.Setter;
 public class Acao {
 
     @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
     private Long id;
 
+    @NotBlank( message = "Não foi informada o título da ação!" )
     private String titulo;
 
+    @NotBlank( message = "Não foi informada a descrição da ação!" )
     private String descricao;
 
+    @NotNull( message = "Não foi informado o local onde a ação é realizada!" )
     private Point localizacao;
 
     @Column( name = "data_inicio" )
+    @NotNull( message = "Não foi informada a data de início do ação!" )
     private LocalDate dataInicio;
 
     @Column( name = "data_fim" )
@@ -45,14 +54,17 @@ public class Acao {
 
     @ManyToOne
     @JoinColumn( name = "id_meta" )
+    @NotNull( message = "Não foi informada uma meta para a ação!" )
     private Meta meta;
 
     @OneToOne
     @JoinColumn( name = "id_coordenador" )
+    @NotNull( message = "Não foi informado um coordenador para essa ação!" )
     private Coordenador coordenador;
 
     @ManyToOne
     @JoinColumn( name = "id_estrutura_organizacional" )
+    @NotNull( message = "Não foi informada qual a estrutura organizacional relacionada com a ação!" )
     private EstruturaOrganizacional estruturaOrganizacional;
 
 }
--- src/main/java/br/ufes/willcq/scpods/domain/service/impl/CadastroAcaoServiceImpl.java
+++ src/main/java/br/ufes/willcq/scpods/domain/service/impl/CadastroAcaoServiceImpl.java
@@ -20,32 +20,8 @@ public class CadastroAcaoServiceImpl implements CadastroAcaoService {
     @Autowired
     private CoordenadorRepository coordenadorRepository;
 
-    // TODO: utilizar o spring-boot-starter-validation para adicionar as validações
-    // diretamente na model
-    private void validarAcao( Acao acao ) {
-
-        if( acao.getMeta() == null && acao.getMeta().getId() == null ) {
-            throw new NegocioException( "Não foi informada uma meta para essa ação!" );
-        }
-
-        if( acao.getCoordenador() == null ) {
-            throw new NegocioException( "Não foi informado um coordenador para essa ação!" );
-        }
-
-        if( acao.getDataInicio() == null ) {
-            throw new NegocioException( "Não foi informada a data de início do projeto!" );
-        }
-
-        if( acao.getEstruturaOrganizacional() == null ) {
-            throw new NegocioException( "Não foi informada qual a estrutura organizacional relacionada com a ação!" );
-        }
-
-    }
-
     @Override
     public Acao salvar( Acao acao ) {
-
-        this.validarAcao( acao );
 
         var optAcao = acaoRepository.findByTitulo( acao.getTitulo() );
         if( optAcao.isPresent() ) {
