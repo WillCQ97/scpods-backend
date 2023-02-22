4132f84c51e2bf2e80036bc2484b796be29dbeb0
WillCQ97 <willian.cqueiroz@gmail.com>
2023-02-22 00:32:59 -0300

    (feat) usando ponto e vírgula como separador dos parâmetros

--- src/main/java/br/ufes/willcq/scpods/api/util/AcaoSearchCriteriaHandler.java
+++ src/main/java/br/ufes/willcq/scpods/api/util/AcaoSearchCriteriaHandler.java
@@ -12,8 +12,8 @@ public class AcaoSearchCriteriaHandler {
 
         if( search != null ) {
 
-            Pattern pattern = Pattern.compile( "(\\w+?)([:<>])(\\w+?),", Pattern.UNICODE_CHARACTER_CLASS );
-            Matcher matcher = pattern.matcher( search + "," );
+            Pattern pattern = Pattern.compile( "(\\w+?)([:<>])(\\w+?);", Pattern.UNICODE_CHARACTER_CLASS );
+            Matcher matcher = pattern.matcher( search + ";" );
 
             while( matcher.find() ) {
                 params.add( new SearchCriteria( matcher.group( 1 ), matcher.group( 2 ), matcher.group( 3 ) ) );
