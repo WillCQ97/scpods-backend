0b9d15198e29af01ce752f6aa1d9134a4748b9d5
WillCQ97 <willian.cqueiroz@gmail.com>
2023-11-11 12:23:24 -0300

    (feat) configurando cors para o localhost

--- src/main/java/br/ufes/willcq/scpods/SistemaCadastroProjetosODSApplication.java
+++ src/main/java/br/ufes/willcq/scpods/SistemaCadastroProjetosODSApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SistemaCadastroProjetosODSApplication {
 
-	public static void main( String[] args ) {
-		SpringApplication.run( SistemaCadastroProjetosODSApplication.class, args );
-	}
+    public static void main( String[] args ) {
+        SpringApplication.run( SistemaCadastroProjetosODSApplication.class, args );
+    }
 
 }
