2d447b0f87e29ba208a8bc08a17266690188ec33
WillCQ97 <willian.cqueiroz@gmail.com>
2023-11-12 17:17:29 -0300

    (feat) alterando as contabilizações para serem baseadas nas ações aceitas

--- src/main/java/br/ufes/willcq/scpods/domain/model/Lotacao.java
+++ src/main/java/br/ufes/willcq/scpods/domain/model/Lotacao.java
@@ -10,6 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -48,20 +49,40 @@ public class Lotacao {
     @OneToMany( mappedBy = "lotacao" )
     private List<Acao> acoes;
 
+    @Transient
+    private List<Acao> acoesAtivas;
+
+    @Transient
+    private List<Acao> submissoes;
+
+    public List<Acao> getAcoesAtivas() {
+        if( acoesAtivas == null || acoesAtivas.isEmpty() ) {
+            acoesAtivas = acoes.stream().filter( acao -> acao.getAceito() ).collect( Collectors.toList() );
+        }
+        return acoesAtivas;
+    }
+
+    public List<Acao> getSubmissoes() {
+        if( submissoes == null || submissoes.isEmpty() ) {
+            submissoes = acoes.stream().filter( acao -> !acao.getAceito() ).collect( Collectors.toList() );
+        }
+        return submissoes;
+    }
+
     public Long getQuantidadeProjetosTotais() {
-        return Long.valueOf( acoes.size() );
+        return Long.valueOf( this.getAcoesAtivas().size() );
     }
 
     public Long getQuantidadeProjetosAtivos() {
-        return acoes.stream().filter( acao -> acao.getDataCadastro() != null ).count();
+        return this.getAcoesAtivas().stream().filter( acao -> acao.getDataEncerramento() != null ).count();
     }
 
     public Long getQuantidadeObjetivosAtendidos() {
-        return acoes.stream().map( acao -> acao.getIdObjetivo() ).distinct().count();
+        return this.getAcoesAtivas().stream().map( acao -> acao.getIdObjetivo() ).distinct().count();
     }
 
     public Long getIdObjetivoMaisAtendido() {
-        var contagemAcoes = acoes.stream().map( acao -> acao.getIdObjetivo() ).collect( Collectors.groupingBy( e -> e, Collectors.counting() ) );
+        var contagemAcoes = this.getAcoesAtivas().stream().map( acao -> acao.getIdObjetivo() ).collect( Collectors.groupingBy( e -> e, Collectors.counting() ) );
         var idOdsMaisAtendido = contagemAcoes.entrySet().stream().max( Map.Entry.comparingByValue() );
 
         return idOdsMaisAtendido.isPresent() ? idOdsMaisAtendido.get().getKey() : null;
