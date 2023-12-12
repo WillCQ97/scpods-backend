a60d59b863cb25987aa86f2557b6ded4816a5bc5
WillCQ97 <willian.cqueiroz@gmail.com>
2023-12-11 22:15:46 -0300

    Atualizações

--- podman.sh
+++ podman.sh
@@ -1,3 +1,7 @@
+# Obtendo a última versão da imagem do postgres com postgis
+podman pull docker.io/postgis/postgis:latest
+
+# Execução do contêiner com as configurações padrão
 podman run -d \
     --name postgis_ods \
     -e POSTGRES_PASSWORD=admin.123 \
