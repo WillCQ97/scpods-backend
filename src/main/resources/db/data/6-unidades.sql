fe35a7f53a18201abd39491b4092d28080c8519f
WillCQ97 <willian.cqueiroz@gmail.com>
2024-05-02 21:51:09 -0300

    chore: removendo dumps antigos e ajustando scripts de dados fixos

--- src/main/resources/db/data/6-unidades.sql
+++ src/main/resources/db/data/6-unidades.sql
@@ -1,24 +1,12 @@
 INSERT INTO public.tb_unidades
-(id, nome, campus)
-VALUES(6, 'Unidade em Jerônimo Monteiro', 'ALEGRE');
-INSERT INTO public.tb_unidades
-(id, nome, campus)
-VALUES(5, 'Área Experimental em Rive, Alegre', 'ALEGRE');
-INSERT INTO public.tb_unidades
-(id, nome, campus)
-VALUES(7, 'Área Experimental em Jerônimo Monteiro', 'ALEGRE');
-INSERT INTO public.tb_unidades
-(id, nome, campus)
-VALUES(8, 'Área Experimental em São José do Calçado', 'ALEGRE');
-INSERT INTO public.tb_unidades
-(id, nome, campus)
-VALUES(2, 'Campus de Goiabeiras', 'GOIABEIRAS');
-INSERT INTO public.tb_unidades
-(id, nome, campus)
-VALUES(1, 'Campus Alegre', 'ALEGRE');
-INSERT INTO public.tb_unidades
-(id, nome, campus)
-VALUES(3, 'Campus Maruípe', 'MARUIPE');
-INSERT INTO public.tb_unidades
-(id, nome, campus)
-VALUES(4, 'Campus São Mateus', 'SAO_MATEUS');
+    (id, nome, campus)
+VALUES
+    (1, 'Campus Alegre', 'ALEGRE'),
+    (2, 'Campus de Goiabeiras', 'GOIABEIRAS'),
+    (3, 'Campus Maruípe', 'MARUIPE'),
+    (4, 'Campus São Mateus', 'SAO_MATEUS'),
+    (6, 'Unidade em Jerônimo Monteiro', 'ALEGRE'),
+    (5, 'Área Experimental em Rive, Alegre', 'ALEGRE'),
+    (7, 'Área Experimental em Jerônimo Monteiro', 'ALEGRE'),
+    (8, 'Área Experimental em São José do Calçado', 'ALEGRE')
+;
