86d140f495a917e285f8afe6d4fdb1d9a35d87dd
WillCQ97 <willian.cqueiroz@gmail.com>
2024-05-27 07:11:32 -0300

    Revert "feat: add segurança com uso de api key secret no header da requisição"
    This reverts commit 7fc1e879b95543694cec924b7debebf311c94a9d.

--- src/main/java/br/ufes/willcq/scpods/SistemaCadastroProjetosODSApplication.java
+++ src/main/java/br/ufes/willcq/scpods/SistemaCadastroProjetosODSApplication.java
@@ -2,10 +2,8 @@ package br.ufes.willcq.scpods;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication( exclude = { SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class } )
+@SpringBootApplication
 public class SistemaCadastroProjetosODSApplication {
 
     public static void main( String[] args ) {
