4902bf5195e7a4c53180ad25a98d53021baf2579
WillCQ97 <willian.cqueiroz@gmail.com>
2024-08-10 18:09:09 -0300

    feat: adicionando listagem das opções de unidades

--- src/main/java/br/ufes/willcq/scpods/domain/repository/UnidadeRepository.java
+++ src/main/java/br/ufes/willcq/scpods/domain/repository/UnidadeRepository.java
@@ -3,8 +3,10 @@ package br.ufes.willcq.scpods.domain.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
+import br.ufes.willcq.scpods.api.dto.select.SelectModel;
 import br.ufes.willcq.scpods.domain.model.Unidade;
 import br.ufes.willcq.scpods.domain.model.enums.CampusEnum;
 
@@ -13,4 +15,8 @@ public interface UnidadeRepository extends ListCrudRepository<Unidade, Long> {
     List<Unidade> findByCampus( CampusEnum campus );
 
     Optional<Unidade> findByCodigo( String codigo );
+
+    @Query( "SELECT u.codigo as value, u.nome as description FROM Unidade u" )
+    List<SelectModel<String>> listarOpcoesUnidades();
+
 }
