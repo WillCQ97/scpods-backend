0f38cde1b2cff9e6cf77310e63fa8d80a71d16d3
WillCQ97 <willian.cqueiroz@gmail.com>
2024-08-11 15:30:02 -0300

    chore: configurando dockerfile, add docker-compose de exemplo e add script build com podman

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
-# todo: FINALIZAR A CRIAÇÃO DO DEPLOY VIA CONTAINER
 FROM docker.io/openjdk:21-jdk-bullseye
 MAINTAINER WillCQ97
 
-#ARG DATABASE_SERVER=localhost
-#ARG DATABASE_PORT=5432
-#ARG DATABASE_NAME=acoes_db
-#ARG DATABASE_USER=postgres
-#ARG DATABASE_PASSWORD=admin.123
-
 EXPOSE 8080
-WORKDIR /app
-COPY target/scpods-backend-1.0.0-SNAPSHOT.jar scpods-backend-1.0.0-SNAPSHOT.jar
-ENTRYPOINT ["java","-jar","/app/scpods-backend-1.0.0-SNAPSHOT.jar"]
+WORKDIR /api-scpods-backend/
+
+COPY target/scpods-api-1.0.0-SNAPSHOT.jar scpods-api-1.0.0-SNAPSHOT.jar
+
+ENTRYPOINT ["java","-jar","/api-scpods-backend/scpods-api-1.0.0-SNAPSHOT.jar"]
---
+++ build-spcods.sh
@@ -0,0 +1,38 @@
+# Script bash contendo os comandos com podman para o build de cada projeto e,
+# em seguida, criar as imagens dos contêineres considerando o diretório atual scpods-backend
+# Exemplo com docker-compose em src/main/resource/docker-compose.yml
+
+cd ..
+cd scpods-frontend
+yarn build
+podman build -t willcq97/scpods-frontend:2.0.0 .
+
+cd ..
+cd scpods-backend
+./mvnw clean package -DskipTests
+podman build -t willcq97/scpods-api:1.0.0 .
+
+cd ..
+
+POD_NAME=scpods-pod
+podman pod create --name $POD_NAME -p 8080:8080 -p 3000:3000 -p 5432:5432
+
+podman run -d \
+    --pod $POD_NAME \
+    --name postgis-acoes-db \
+    -e POSTGRES_DB=acoes_db \
+    -e POSTGRES_PASSWORD=admin.123 \
+    --restart always \
+    -v acoes-db-data:/var/lib/postgresql/data \
+    docker.io/postgis/postgis:16-3.4
+
+podman run -d \
+    --pod $POD_NAME \
+    --name scpods-api \
+    willcq97/scpods-api:1.0.0
+
+podman run -d \
+    --pod $POD_NAME \
+    --name scpods-fronted \
+    willcq97/scpods-frontend:2.0.0
+
