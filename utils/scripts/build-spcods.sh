20460595f69b49596f4ca12211f884588f80cc52
WillCQ97 <willian.cqueiroz@gmail.com>
2024-08-22 21:25:14 -0300

    chore: revisando nomenclatura e descrição dos projetos

--- utils/scripts/build-spcods.sh
+++ utils/scripts/build-spcods.sh
@@ -10,5 +10,5 @@ podman build --platform linux/arm64 -t willcq97/scpods-api:1.0.0-arm64 .
 cd ..
 cd scpods-frontend
 yarn build
-podman build --platform linux/amd64 -t willcq97/scpods-frontend:1.0.0-amd64 .
-podman build --platform linux/arm64 -t willcq97/scpods-frontend:1.0.0-arm64 .
+podman build --platform linux/amd64 -t willcq97/scpods-site:1.0.0-amd64 .
+podman build --platform linux/arm64 -t willcq97/scpods-site:1.0.0-arm64 .
