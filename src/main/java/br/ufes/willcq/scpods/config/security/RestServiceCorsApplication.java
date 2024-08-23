a27cbbaaf6ee743970a5d3d3c4b51235a26cccd7
WillCQ97 <willian.cqueiroz@gmail.com>
2024-08-23 00:54:51 -0300

    Revert "chore: permitindo cors do localhost:3000"
    This reverts commit 29e4cc8f4b3b094cf029b092a0ae62d99ceb8c95.

--- src/main/java/br/ufes/willcq/scpods/config/security/RestServiceCorsApplication.java
+++ src/main/java/br/ufes/willcq/scpods/config/security/RestServiceCorsApplication.java
@@ -14,10 +14,11 @@ public class RestServiceCorsApplication {
 
         return new WebMvcConfigurer() {
 
+            // TODO: refinar o acesso a estes métodos
             @Override
             public void addCorsMappings( @NonNull CorsRegistry registry ) {
                 registry.addMapping( "/**" )
-                        .allowedOrigins( "http://localhost:3000" )
+                        .allowedOrigins( "http://localhost", "http://localhost:80", "http://localhost:3000", "http://localhost:8080", "http://localhost:8001" )
                         .allowedMethods( "HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS" );
             }
         };
