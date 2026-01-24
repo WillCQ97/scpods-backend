b13bf9776cb9ba500473755ae6f2d016801e4f94
WillCQ97 <willian.cqueiroz@gmail.com>
2026-01-24 13:42:03 -0300

    refactor: usa record e atualiza comentário

--- src/main/java/com/github/willcq97/scpods/api/dto/select/SelectModel.java
+++ src/main/java/com/github/willcq97/scpods/api/dto/select/SelectModel.java
@@ -1,8 +1,10 @@
 package com.github.willcq97.scpods.api.dto.select;
 
-// todo: escrever em português
-// A model representing a selectable option with a value and description.
-// Used in dropdowns, radio buttons, etc.
+/**
+ * Um modelo representando uma opção selecionável com um valor e descrição. Utilizado em dropdowns, botões de rádio, etc.
+ * 
+ * @param <T> o tipo do valor da opção selecionável
+ */
 public interface SelectModel<T> {
 
     T getValue();
