6acf6b7e7f8d0ca2c67c4487caff903881bec7b8
WillCQ97 <willian.cqueiroz@gmail.com>
2026-03-17 23:25:25 -0300

    refactor: remove uso do modelmapper

--- src/main/java/com/github/willcq97/scpods/api/mapper/AcaoMapper.java
+++ src/main/java/com/github/willcq97/scpods/api/mapper/AcaoMapper.java
@@ -1,10 +1,13 @@
 package com.github.willcq97.scpods.api.mapper;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import com.github.willcq97.scpods.api.dto.input.SubmissaoInputDTO;
 import com.github.willcq97.scpods.domain.model.entity.Acao;
+import com.github.willcq97.scpods.domain.model.entity.Coordenador;
+import com.github.willcq97.scpods.domain.model.entity.Local;
+import com.github.willcq97.scpods.domain.model.entity.Lotacao;
+import com.github.willcq97.scpods.domain.model.entity.Meta;
 
 import lombok.AllArgsConstructor;
 
@@ -12,10 +15,36 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class AcaoMapper {
 
-    private final ModelMapper modelMapper;
+    public Acao toEntity( SubmissaoInputDTO dto ) {
+        var acao = new Acao();
 
-    public Acao mapToEntity( SubmissaoInputDTO dto ) {
-        return modelMapper.map( dto, Acao.class );
+        acao.setTitulo( dto.getTitulo() );
+        acao.setDescricao( dto.getDescricao() );
+        acao.setUrlEvidencia( dto.getUrlEvidencia() );
+        acao.setDataInicio( dto.getDataInicio() );
+        acao.setDataEncerramento( dto.getDataEncerramento() );
+
+        var meta = new Meta();
+        meta.setId( dto.getMetaId() );
+        acao.setMeta( meta );
+
+        var local = new Local();
+        local.setId( dto.getLocalId() );
+        acao.setLocal( local );
+
+        var lotacao = new Lotacao();
+        lotacao.setId( dto.getLotacaoId() );
+        acao.setLotacao( lotacao );
+
+        var coordDto = dto.getCoordenador();
+        var coordenador = new Coordenador();
+        coordenador.setNome( coordDto.getNome() );
+        coordenador.setEmail( coordDto.getEmail() );
+        coordenador.setTipoVinculo( coordDto.getTipoVinculo() );
+        coordenador.setDescricaoVinculo( coordDto.getDescricaoVinculo() );
+        acao.setCoordenador( coordenador );
+
+        return acao;
     }
 
 }
