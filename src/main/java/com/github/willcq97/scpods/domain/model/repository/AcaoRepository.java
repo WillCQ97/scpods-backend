d0943a548c34984cc30430e86faf69b606b49b3d
WillCQ97 <willian.cqueiroz@gmail.com>
2026-03-17 23:01:01 -0300

    chore: upgrade spring boot para versão 4
    ```
    mvn -U org.openrewrite.maven:rewrite-maven-plugin:run -Drewrite.recipeArtifactCoordinates=org.openrewrite.recipe:rewrite-spring:RELEASE -Drewrite.activeRecipes=org.openrewrite.java.spring.boot4.UpgradeSpringBoot_4_0 -Drewrite.exportDatatables=true
    ```
    
    https://docs.openrewrite.org/recipes/java/spring/boot4/upgradespringboot_4_0-community-edition

--- src/main/java/com/github/willcq97/scpods/domain/model/repository/AcaoRepository.java
+++ src/main/java/com/github/willcq97/scpods/domain/model/repository/AcaoRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.repository.ListCrudRepository;
 
 import com.github.willcq97.scpods.domain.model.entity.Acao;
@@ -19,7 +19,7 @@ public interface AcaoRepository extends ListCrudRepository<Acao, Long>, JpaSpeci
     List<Acao> findByAceito( Boolean aceito );
 
     @Modifying
-    @Query( nativeQuery = true, value = "UPDATE tb_acoes SET fl_aceito = TRUE WHERE id = :idAcao" )
+    @NativeQuery("UPDATE tb_acoes SET fl_aceito = TRUE WHERE id = :idAcao")
     void aceitarSubmissao( Long idAcao );
 
 }
