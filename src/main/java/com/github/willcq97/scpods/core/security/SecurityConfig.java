b84da650ba26d51d7cdcedd2afb4bb5e5b4e5a2e
WillCQ97 <willian.cqueiroz@gmail.com>
2026-03-23 22:32:58 -0300

    fix: corrige paths nâo iniciados com barra

--- src/main/java/com/github/willcq97/scpods/core/security/SecurityConfig.java
+++ src/main/java/com/github/willcq97/scpods/core/security/SecurityConfig.java
@@ -35,8 +35,8 @@ public class SecurityConfig {
         http.authorizeHttpRequests(
                 expressionIntercepterUrlRegistry -> expressionIntercepterUrlRegistry
                         .requestMatchers( "/api-docs/**", "/swagger-ui/**" ).permitAll()
-                        .requestMatchers( "v1/**", "v1/usuarios/validar-login-ufes" ).permitAll()
-                        .requestMatchers( "v1/submissoes/**", "v1/usuarios/**" ).authenticated() )
+                        .requestMatchers( "/v1/**", "/v1/usuarios/validar-login-ufes" ).permitAll()
+                        .requestMatchers( "/v1/submissoes/**", "/v1/usuarios/**" ).authenticated() )
                 .csrf( csrf -> csrf.disable() )
                 .httpBasic( Customizer.withDefaults() );
         return http.build();
